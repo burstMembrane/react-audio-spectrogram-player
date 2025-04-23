aa3688b8989c00c3ccaa310404bc8be2c60b3f59
Liam Power <liamfpower@gmail.com>
2025-04-23 10:35:22 +1000

    fix: types

---
+++ src/lib/index.d.ts
@@ -0,0 +1,39 @@
+import { ReactNode } from 'react';
+
+export interface Annotations {
+    data: (string | number)[][];
+    title?: string;
+    height?: number;
+    strokeWidth?: number;
+}
+
+export interface SpectrogramPlayerProps {
+    src: string;
+    sxx?: number[][];
+    sampleRate?: number;
+    n_fft?: number;
+    win_length?: number;
+    hop_length?: number;
+    f_min?: number;
+    f_max?: number;
+    n_mels?: number;
+    top_db?: number;
+    annotations?: Annotations[];
+    navigator?: boolean;
+    settings?: boolean;
+    startTimeInitial?: number;
+    endTimeInitial?: number;
+    playbackSpeedInitial?: number;
+    playheadModeInitial?: string;
+    specHeight?: number;
+    navHeight?: number;
+    colormap?: string;
+    transparent?: boolean;
+    dark?: boolean;
+    playheadColor?: string;
+    playheadWidth?: number;
+}
+
+declare const SpectrogramPlayer: (props: SpectrogramPlayerProps) => JSX.Element;
+
+export default SpectrogramPlayer; 
